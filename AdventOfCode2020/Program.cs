1309c0144e0c9f8e32dd7387fae404400902276e
Mike <xxmixanikxx@gmail.com>
2020-12-04 00:26:04 +0200

    Day 3 solutions & little refactor

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -9,15 +9,18 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            Puzzle1(); // passed
-            Puzzle2(); // passed
-            Puzzle3(); // passed
-            Puzzle4(); // passed
+            Puzzle1a(); // passed
+            Puzzle1b(); // passed
+            Puzzle2a(); // passed
+            Puzzle2b(); // passed
+            Console.WriteLine("Trees(1): " + Puzzle3a(3, 1)); // passed
+            Puzzle3b(); // passed
+
 
             Console.ReadKey();
         }
 
-        public static void Puzzle1()
+        public static void Puzzle1a()
         {
             List<int> nums = File.ReadAllLines("./puzzleData1/numbers.txt").ToList().Select(int.Parse).ToList();
 
@@ -25,14 +28,14 @@ namespace AdventOfCode2020
             {
                 if (nums.Contains(2020 - num))
                 {
-                    Console.Write("Answer: ");
-                    Console.WriteLine(num * (2020 - num));
+                    int result = num * (2020 - num);
+                    Console.WriteLine("Result(1): " + result);
                     return;
                 }
             }
         }
 
-        public static void Puzzle2()
+        public static void Puzzle1b()
         {
             List<int> nums = File.ReadAllLines("./puzzleData1/numbers.txt").ToList().Select(int.Parse).ToList();
 
@@ -44,15 +47,15 @@ namespace AdventOfCode2020
                 {
                     if (nums.Contains(tempNum - nums[j]) && i != j)
                     {
-                        Console.Write("Answer: ");
-                        Console.WriteLine(nums[i] * nums[j] * (tempNum - nums[j]));
+                        int result = nums[i] * nums[j] * (tempNum - nums[j]);
+                        Console.WriteLine("Result(2): " + result);
                         return;
                     }
                 }
             }
         }
 
-        public static void Puzzle3()
+        public static void Puzzle2a()
         {
             List<String> passwordPolicies = File.ReadAllLines("./puzzleData2/passwords.txt").ToList();
             int valid = 0;
@@ -67,10 +70,10 @@ namespace AdventOfCode2020
 
                 if (count >= amounts[0] && count <= amounts[1]) valid += 1;
             }
-            Console.WriteLine("Answer: " + valid);
+            Console.WriteLine("Passwords Valid(1): " + valid);
         }
 
-        public static void Puzzle4()
+        public static void Puzzle2b()
         {
             List<String> passwordPolicies = File.ReadAllLines("./puzzleData2/passwords.txt").ToList();
             int valid = 0;
@@ -85,7 +88,34 @@ namespace AdventOfCode2020
 
                 if (count == 1) valid += 1;
             }
-            Console.WriteLine("Answer: " + valid);
+            Console.WriteLine("Passwords Valid(2): " + valid);
+        }
+
+        public static int Puzzle3a(int stepX, int stepY)
+        {
+            List<String> treePatterns = File.ReadAllLines("./puzzleData3/trees.txt").ToList();
+            int trees = 0;
+
+            for (int y = 0, x = 0; y < treePatterns.Count; y += stepY, x += stepX)
+            {
+                if (treePatterns[y][x % treePatterns[y].Length] == '#') trees += 1;
+            }
+            return trees;
+        }
+
+        public static void Puzzle3b()
+        {
+            List<int[]> slopes = new List<int[]>()
+            {
+                new int[]{1, 1},
+                new int[]{3, 1},
+                new int[]{5, 1},
+                new int[]{7, 1},
+                new int[]{1, 2}
+            };
+            List<int> treeResults = slopes.Select(slope => Puzzle3a(slope[0], slope[1])).ToList();
+            int result = treeResults.Aggregate((total, el) => total * el);
+            Console.WriteLine("Trees(2): " + result);
         }
     }
 }
