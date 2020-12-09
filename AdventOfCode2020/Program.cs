55e95926fb50ecf992bfe99d0925b52e9f647496
Mike <xxmixanikxx@gmail.com>
2020-12-09 07:46:55 +0200

    Day 7,8,9 solutions

--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2020
 {
@@ -21,6 +22,12 @@ namespace AdventOfCode2020
             Puzzle5b();
             Puzzle6a();
             Puzzle6b();
+            Puzzle7a();
+            Puzzle7b();
+            Puzzle8a();
+            Puzzle8b();
+            Puzzle9a();
+            Puzzle9b();
 
             Console.ReadKey();
         }
@@ -244,6 +251,175 @@ namespace AdventOfCode2020
             }).ToList();
 
             Console.WriteLine("Forms(2): " + uniqueGroupAnswers.Sum());
+        }
+
+        public static void Puzzle7a()
+        {
+            List<String> bagRulesData = File.ReadAllLines("./puzzleData/bags.txt").ToList();
+            Dictionary<String, List<String>> bagRelations = new Dictionary<string, List<string>>();
+            Regex pattern = new Regex(@"\b(?!bags?|contain|,|\d)\b(\S+ \S+)");
+            bagRulesData.ForEach(rd => {
+                List<String> matches = pattern.Matches(rd).Cast<Match>().Select(m => m.Value).ToList();
+                bagRelations[matches[0]] = matches.Skip(1).ToList();
+            });
+            int count = 0;
+
+            foreach (var contains in bagRelations)
+            {
+                if (ContainsBag(bagRelations, contains.Value, "shiny gold")) count++;
+            }
+            Console.WriteLine("Bags(1): " + count);
+        }
+
+        public static void Puzzle7b()
+        {
+            List<String> bagRulesData = File.ReadAllLines("./puzzleData/bags.txt").ToList();
+            Dictionary<String, List<String[]>> bagRelations = new Dictionary<String, List<String[]>>();
+            Regex pattern = new Regex(@"\b(?!bags?|contain|,)\b((\d )?\S+ \S+)");
+            bagRulesData.ForEach(rd => {
+                List<String> matches = pattern.Matches(rd).Cast<Match>().Select(m => m.Value).ToList();
+                bagRelations[matches[0]] = matches.Skip(1).Select(m => m.Split(new[] {' '}, 2)).ToList();
+            });
+
+            Console.WriteLine("Bags(2): " + CountInnerBags(bagRelations, "shiny gold"));
+        }
+
+        public static void Puzzle8a()
+        {
+            List<String> operationsData = File.ReadAllLines("./puzzleData/operations.txt").ToList();
+            List<int> usedOpIndexes = new List<int>();
+            int acc = 0;
+            int opIndex = 0;
+
+            while(!usedOpIndexes.Contains(opIndex) && opIndex >= 0 && opIndex < operationsData.Count - 1)
+            {
+                string[] opParts = operationsData[opIndex].Split(' ');
+                usedOpIndexes.Add(opIndex);
+
+                switch (opParts[0])
+                {
+                    case "nop":
+                        opIndex += 1;
+                        break;
+                    case "acc":
+                        acc += int.Parse(opParts[1]);
+                        opIndex += 1;
+                        break;
+                    case "jmp":
+                        opIndex += int.Parse(opParts[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("Operations(1): " + acc);
+        }
+
+        public static void Puzzle8b()
+        {
+            List<String> operationsData = File.ReadAllLines("./puzzleData/operations.txt").ToList();
+            List<int> usedOpIndexes = new List<int>();
+            List<int> triedModifications = new List<int>();
+            int acc = 0;
+            int opIndex = 0;
+            bool canChange = true;
+
+            while (opIndex >= 0 && opIndex < operationsData.Count-1)
+            {
+                if (usedOpIndexes.Contains(opIndex))
+                {
+                    acc = 0;
+                    opIndex = 0;
+                    usedOpIndexes = new List<int>();
+                    canChange = true;
+                }
+
+                string[] opParts = operationsData[opIndex].Split(' ');
+                usedOpIndexes.Add(opIndex);
+
+                if ((opParts[0] == "nop" || opParts[0] == "jmp") && !triedModifications.Contains(opIndex) && canChange)
+                {
+                    opParts[0] = (opParts[0] == "nop") ? "jmp" : "nop";
+                    triedModifications.Add(opIndex);
+                    canChange = false;
+                }
+
+                switch (opParts[0])
+                {
+                    case "nop":
+                        opIndex += 1;
+                        break;
+                    case "acc":
+                        acc += int.Parse(opParts[1]);
+                        opIndex += 1;
+                        break;
+                    case "jmp":
+                        opIndex += int.Parse(opParts[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("Operations(2): " + acc);
+        }
+
+        public static void Puzzle9a()
+        {
+            List<long> nums = File.ReadAllLines("./puzzleData/xmas.txt").ToList().Select(long.Parse).ToList();
+            int preamble = 25;
+
+            for (int i = preamble; i < nums.Count; i++)
+            {
+                List<long> preambleNums = nums.GetRange(i-preamble, preamble);
+                bool found = false;
+
+                for (int j = 0; j < preambleNums.Count; j++)
+                {
+                    for (int k = 0; k < preambleNums.Count; k++)
+                    {
+                        if (preambleNums[j] + preambleNums[k] == nums[i])
+                        {
+                            found = true;
+                        }
+                    }
+                }
+                if(!found) Console.WriteLine("XMAS(1): " + nums[i]);
+            }
+        }
+
+        public static void Puzzle9b()
+        {
+            List<long> nums = File.ReadAllLines("./puzzleData/xmas.txt").ToList().Select(long.Parse).ToList();
+            int invalidNum = 31161678;
+
+            for (int i = 2; i < nums.Count; i++)
+            {
+                for (int j = 0; j < nums.Count-i; j++)
+                {
+                    List<long> contiguousSet = nums.GetRange(j, i);
+                    long sum = contiguousSet.Sum();
+                    long weakness = contiguousSet.Min() + contiguousSet.Max();
+                    if (sum == invalidNum) Console.WriteLine("XMAS(2): " + weakness);
+                }
+            }
+        }
+
+
+
+        // Helper Functions
+        public static bool ContainsBag(Dictionary<String, List<String>> relations, List<String> bagBags, string bagName)
+        {
+            if (bagBags.Contains(bagName)) return true;
+            if (bagBags.Count == 1 && bagBags[0] == "no other") return false;
+
+            return bagBags.Select(bag => ContainsBag(relations, relations[bag], bagName)).Any(x => x == true);
+        }
+
+        public static int CountInnerBags(Dictionary<String, List<String[]>> relations, string bagName)
+        {
+            if (relations[bagName].Count == 1 && string.Join(" ", relations[bagName][0]) == "no other") return 0;
+
+            return relations[bagName].Select(bag => int.Parse(bag[0]) + int.Parse(bag[0]) * CountInnerBags(relations, bag[1])).Sum();
         }
     }
 }
